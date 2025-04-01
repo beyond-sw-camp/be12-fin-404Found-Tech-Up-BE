282f1ec32cd57a74cafc49795eaceaac19754bbe
Xen-alpha <hky7548@naver.com>
2025-04-01 15:16:10 +0900

    [Docs] add some swagger description to user controller

--- backend/src/main/java/com/example/backend/user/controller/UserController.java
+++ backend/src/main/java/com/example/backend/user/controller/UserController.java
@@ -1,10 +1,33 @@
 package com.example.backend.user.controller;
 
 
+import com.example.backend.user.model.dto.SignupRequest;
+import com.example.backend.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name="회원 관련 기능", description="회원 가입/로그인")
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/user")
 public class UserController {
+    private final UserService userService;
+
+    @Operation(summary="회원가입", description = "회원 가입을 합니다")
+    @ApiResponse(responseCode="200", description="정상가입, 성공 문자열을 반환합니다.")
+    @ApiResponse(responseCode="400", description="가입 실패")
+    @ApiResponse(responseCode="500", description="서버 내 오류")
+    @PostMapping("/signup")
+    private ResponseEntity<String> signup(@Valid SignupRequest request) {
+        return ResponseEntity.ok("Signup success");
+    }
+
+
 }
--- backend/src/main/java/com/example/backend/user/model/dto/SignupRequest.java
+++ backend/src/main/java/com/example/backend/user/model/dto/SignupRequest.java
@@ -1,5 +1,8 @@
 package com.example.backend.user.model.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,8 +10,14 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@Schema(description="회원가입 정보 Request")
 public class SignupRequest {
+    @Schema(description="별명, 필수",required = true,  example = "Yippie20")
+    @NotBlank
     private String nickname;
+    @Schema(description="이메일, 필수",required = true,  example = "example@example.com")
+    @Email
+    @NotBlank
     private String email;
     private String validatedString;
     private String password;
