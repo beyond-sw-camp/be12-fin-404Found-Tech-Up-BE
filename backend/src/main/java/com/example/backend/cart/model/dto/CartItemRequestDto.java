249051e5028a0f8f4d6a7d88006cb2a17a46a936
goodleaf96 <ky1234tho@gmail.com>
2025-04-10 10:16:30 +0900

    dto 구현 및 레포지토리 인터페이스로 수정 완료

--- backend/src/main/java/com/example/backend/cart/model/dto/CartItemRequestDto.java
+++ backend/src/main/java/com/example/backend/cart/model/dto/CartItemRequestDto.java
@@ -1,5 +1,8 @@
 package com.example.backend.cart.model.dto;
 
+import com.example.backend.cart.model.CartItem;
+import com.example.backend.product.model.Product;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,5 +13,16 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CartItemRequestDto {
-    private int idx;
+    @Schema(description = "장바구니에 추가된 수량", example = "2")
+    private int cartItemQuantity;
+
+    @Schema(description = "상품 정보")
+    private Product product;
+
+    public CartItem toEntity() {
+        return CartItem.builder()
+                .cartItemQuantity(cartItemQuantity)
+                .product(product)
+                .build();
+    }
 }
