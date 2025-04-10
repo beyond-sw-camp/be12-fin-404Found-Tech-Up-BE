98e931226b264d2abc574ecce1e2f2ad804b804c
Xen-alpha <hky7548@naver.com>
2025-04-10 17:44:44 +0900

    [Fix] 신규 회원 수 받아올 때 오류 수정#BTS-135

--- backend/src/main/java/com/example/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/example/backend/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.backend.config;
 
 import com.example.backend.config.filter.LoginFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -12,6 +13,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
@@ -49,5 +55,4 @@ public class SecurityConfig {
 
         return http.build();
     }
-
 }
