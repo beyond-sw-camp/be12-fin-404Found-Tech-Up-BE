939260f46a5e63ae85cee8a523aa979c1581ef83
gunha0405 <dhrjsgk0405@gmail.com>
2025-04-11 17:19:58 +0900

    BTS-60 [Feat] 게시글 작성 기능 완성 / s3 업로드 기능 구현 완료

--- backend/src/main/java/com/example/backend/board/model/dto/BoardRegisterRequestDto.java
+++ backend/src/main/java/com/example/backend/board/model/dto/BoardRegisterRequestDto.java
@@ -9,6 +9,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
@@ -26,7 +27,7 @@ public class BoardRegisterRequestDto {
     private String boardCategory;
 
     @Schema(description = "게시글 첨부파일 URL 목록", example = "[\"https://s3.amazonaws.com/your-bucket/file1.jpg\", \"https://s3.amazonaws.com/your-bucket/file2.pdf\"]")
-    private List<String> files;  // 프리사인드 URL 방식으로 S3 업로드 후 최종 URL들을 클라이언트에서 전달받음
+    private List<String> files = new ArrayList<>();  // 프리사인드 URL 방식으로 S3 업로드 후 최종 URL들을 클라이언트에서 전달받음
 
     // 로그인한 유저 정보를 받아 Board 엔티티로 변환 (파일 정보는 별도 등록 처리)
     public Board toEntity(User loginUser) {
